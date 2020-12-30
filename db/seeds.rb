001557749f727ac3fa504b1566ea2d12ac7e6c5a
Freedom Scheyd <joefreedomsch@gmail.com>
2020-12-29 22:57:52 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,5 @@
 goldor = User.create(character_name: "Goldor the Red", email: "goldor@dragonslayer.com", password: "bloodforthebloodlord")
 philthanax = User.create(character_name: "Philthanax", email: "Philthanax@ilovedragons.com", password: "chromaticsarentbad")
 
-#goldors_monster = goldor.monsters.build(name: "Beholder", monster_type: "Abberation", weakness: "n/a", cr: "13")
-#goldors_monster.save
-
+goldors_monster = goldor.monsters.build(name: "Beholder", monster_type: "Abberation", weakness: "n/a", cr: "13")
+goldors_monster.save

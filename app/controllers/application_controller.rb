97cacbfc30b6ee35b6e28d2776eb479740ea6068
Freedom Scheyd <joefreedomsch@gmail.com>
2020-12-31 16:47:24 -0500

    enabled sessions, fixed routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,6 @@ class ApplicationController < Sinatra::Base
     
   end
 
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
     
+
+
     #renders login page
     get '/login' do
         erb :login
     end
 
+    post '/login' do
+        @user = User.find_by(character_name: params[:character_name])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            puts session
+            redirect "users/#{@user.id}"
+        else
+        
+        end
+    end
+
+
     get '/signup' do
 
+    end
+
+    get '/users/:id' do
+        "this will be the user show route"
     end
 end

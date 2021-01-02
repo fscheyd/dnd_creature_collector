64b6e2d34b67fac3cdff1537127e16dedfd78ca6
Freedom Scheyd <joefreedomsch@gmail.com>
2021-01-02 12:10:47 -0500

    helper methods on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,17 @@ class ApplicationController < Sinatra::Base
     
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(session[:user_id])
+    end
+
+  end
+
 
 
 end

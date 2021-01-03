9cfc05ebab400e51eb679c7084fa93425da13c05
Freedom Scheyd <joefreedomsch@gmail.com>
2021-01-03 14:53:07 -0500

    built out new.erb form

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,3 +1,17 @@
 class MonstersController < ApplicationController
+    
+    get '/monsters/new' do
+        erb :'/monsters/new'
+    end
 
+    get '/monsters' do
+        authenticate
+        @user = current_user
+        @monsters = monster.all
+        erb :"monsters/show"
+    end
+
+    post '/monsters' do
+
+    end
 end

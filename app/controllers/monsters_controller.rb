f8ac60f31ea67be25ce890b819908564b17e0748
Freedom Scheyd <joefreedomsch@gmail.com>
2021-01-03 15:15:11 -0500

    building out routes for new monster form

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -4,14 +4,23 @@ class MonstersController < ApplicationController
         erb :'/monsters/new'
     end
 
-    get '/monsters' do
-        authenticate
-        @user = current_user
-        @monsters = monster.all
-        erb :"monsters/show"
-    end
+    # get '/monsters' do
+    #     authenticate
+    #     @user = current_user
+    #     @monsters = monster.all
+    #     erb :"monsters/show"
+    # end
 
     post '/monsters' do
+        if !logged_in?
+            redirect '/'
+        end
+
+        if params[:name, :monster_type] != ""
+            @monster = monster.create(name: params[:name], monster_type: params[:monster_type], user_id: current_user.id)
+            redirect "/monsters/#{@monster.id}"
+        else
+            redirect '/monsters/new'
 
     end
 end

71f7643725415b28dd3c22c179340f85fbad0af9
Freedom Scheyd <joefreedomsch@gmail.com>
2021-01-18 12:04:25 -0500

    Moved monster logic from views to controllers

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -5,12 +5,22 @@ class MonstersController < ApplicationController
     end
 
     get '/monsters' do
-        @monsters = Monster.all
-        erb :'/monsters/index'
+        if logged_in? 
+            @monsters = Monster.all
+            @my_monsters = @monsters.select do |monster|
+                monster.user == current_user
+            end    
+            erb :'/monsters/index'
+        else
+            redirect to '/login'
+        end
     end
     
 
     post '/monsters/new' do
+        if !logged_in?
+            redirect '/login'
+        end
         monster = Monster.new(params[:monster])
         if monster.save
             redirect "/monsters/#{monster.id}"
@@ -22,13 +32,28 @@ class MonstersController < ApplicationController
     
 
     get '/monsters/:id' do
-        @monster = Monster.find(params[:id])
-        erb :'/monsters/show'
+        if !logged_in?
+            redirect to '/login'
+        end
+            @monster = Monster.find(params[:id])
+            if @monster.user_id == current_user.id
+                erb :'/monsters/show'
+            else
+                redirect '/monsters'
+            end
     end
 
+  
     get '/monsters/:id/edit' do
+        if !logged_in?
+            redirect '/login'
+        end
         @monster = Monster.find_by(id: params[:id])
-        erb :'/monsters/edit'
+        if @monster.user_id == current_user.id
+            erb :'/monsters/edit'
+        else
+            redirect '/monsters' 
+        end
     end
 
     patch '/monsters/:id' do
